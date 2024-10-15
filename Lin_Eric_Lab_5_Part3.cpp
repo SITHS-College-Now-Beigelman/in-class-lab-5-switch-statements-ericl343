1f559ce5314f202e187c1de6aef87650ec9f997c
ericl343 <ericl343@nycstudents.net>
2024-10-14 22:33:27 -0400

    p3

--- Lin_Eric_Lab_5_Part3.cpp
+++ Lin_Eric_Lab_5_Part3.cpp
@@ -9,37 +9,51 @@
 using namespace std;
 
 int main() {
+    srand(time(0));
+
+//Declaring variables
 int i;
-int ew = 0;
-int ns = 0;
-
-
-srand(time(0));
-    int direct;
+int ew = 0;             //East or West
+int ns = 0;             //North or South
+int moves = 0;          //Number of moves
+int tarX = 2;           // Target coordinates
+int tarY = 3;           // Target coordinates
+int currentX = 0;       // Current X
+int currentY = 0;       // Current Y
 
 for (i = 0 ;i < 25; i++)
 { 
-    direct = rand() % 4 + 1;    //random number between 1 and 4 so each is 25% //probable
-                                //1 means North, 2 means South, 3 means East and 4 means West
-    cout << direct << endl;
+    int direct = rand() % 4 + 1;    //Random number between 1 and 4
+    moves++;
 
-if (direct == 1) 
-{
-    ns = ns + 1;
+//1 means North, 2 means South, 3 means East and 4 means West
+switch (direct) {
+case 1: // North
+currentY++;
+ns++;
+break;
+
+case 2: // South
+currentY--;
+ns--;
+break;
+
+case 3: // East
+currentX++;
+ew++;
+break;
+
+case 4: // West
+currentX--;
+ew--;
+break;
 }
-else if (direct == 2) 
-{
-    ns = ns - 1;
 }
-else if (direct == 3) 
-{
-    ew = ew + 1;
-}
-else if (direct == 4) 
-{
-    ew = ew - 1;
-}
-}
+// Calculate final position
+currentX = ew;
+currentY = ns;
+cout << "Final Position: (" << currentX << ", " << currentY << ")" << endl;
+cout << "Total Moves: " << moves << endl;
  
 if (ns > 0)
 {
@@ -59,35 +73,53 @@ else if (ew < 0)
     cout << "You went west " << abs (ew) << " times!" << endl;
 }
 
+// Reset position for the Extra Credit Part
+currentX = 0;  
+currentY = 0;
+moves = 0;
+
+bool reachD = false;    //Bool used to determine results
+
+while (moves < 25 && !reachD) {
+int direction = rand() % 4 + 1;
+moves++;
+
+switch (direction) 
+{
+case 1: // North
+    currentY++;
+    break;
+case 2: // South
+    currentY--;
+    break;
+case 3: // East
+    currentX++;
+    break;
+case 4: // West
+    currentX--;
+    break;
+}
+
+// See if destination is reached using if statements 
+if (currentX == tarX && currentY == tarY)
+{
+    reachD = true;
+    cout << "Reached destination (2, 3) in " << moves << " moves." << endl;
+}
+}
+
+if (!reachD) 
+{
+    cout << "Didn't reach destination (2, 3) within 25 moves." << endl;
+}
+
 return 0;
 }
 
 /*
-1
-4
-1
-3
-2
-2
-3
-2
-1
-1
-1
-4
-3
-4
-4
-1
-2
-3
-3
-3
-3
-3
-1
-2
-3
-You went north 2 times!
-You went east 5 times!
+Final Position: (-6, 3)
+Total Moves: 25
+You went north 3 times!
+You went west 6 times!
+Didn't reach destination (2, 3) within 25 moves.
 */
